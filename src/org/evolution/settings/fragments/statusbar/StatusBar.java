c5d69147cb4e4dd74591b9a51c3a97883bf1fd64
Pranav Vashi <neobuddy89@gmail.com>
2017-03-19 01:48:25 +0530

    Evolver: Add ability to toggle bluetooth battery level [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id725cf7ed6c26131baf36e826928d907ef6f46d6
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/evolution/settings/fragments/statusbar/StatusBar.java
+++ src/org/evolution/settings/fragments/statusbar/StatusBar.java
@@ -21,11 +21,19 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
+import org.evolution.settings.utils.DeviceUtils;
+
 @SearchIndexable
 public class StatusBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "StatusBar";
+
+    private static final String KEY_ICONS_CATEGORY = "status_bar_icons_category";
+    private static final String KEY_BLUETOOTH_BATTERY_STATUS = "bluetooth_show_battery";
+
+    private PreferenceCategory mIconsCategory;
+    private Preference mBluetoothBatteryStatus;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,6 +43,13 @@ public class StatusBar extends SettingsPreferenceFragment implements
         Context context = getContext();
 
         PreferenceScreen preferenceScreen = getPreferenceScreen();
+
+        mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
+        mBluetoothBatteryStatus = (Preference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
+
+        if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+            mIconsCategory.removePreference(mBluetoothBatteryStatus);
+        }
     }
 
     @Override
@@ -53,6 +68,10 @@ public class StatusBar extends SettingsPreferenceFragment implements
             @Override
             public List<String> getNonIndexableKeys(Context context) {
                 List<String> keys = super.getNonIndexableKeys(context);
+
+                if (!DeviceUtils.deviceSupportsBluetooth(context)) {
+                    keys.add(KEY_BLUETOOTH_BATTERY_STATUS);
+                }
                 return keys;
             }
         };
