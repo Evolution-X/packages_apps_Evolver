9b03fb900bcaf5c27cdcb97b99342c9a8bb01dde
Varun Date <date.varun123@gmail.com>
2024-05-12 20:48:42 +0900

    Evolver: Option to Display Data Disabled Indicator Icon [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/evolution/settings/fragments/statusbar/StatusBar.java
+++ src/org/evolution/settings/fragments/statusbar/StatusBar.java
@@ -35,6 +35,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String KEY_QUICK_PULLDOWN = "qs_quick_pulldown";
 
     private static final String KEY_ICONS_CATEGORY = "status_bar_icons_category";
+    private static final String KEY_DATA_DISABLED_ICON = "data_disabled_icon";
     private static final String KEY_BLUETOOTH_BATTERY_STATUS = "bluetooth_show_battery";
 
     private static final int PULLDOWN_DIR_NONE = 0;
@@ -45,6 +46,7 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private LineageSystemSettingListPreference mQuickPulldown;
 
     private PreferenceCategory mIconsCategory;
+    private Preference mDataDisabledIcon;
     private Preference mBluetoothBatteryStatus;
 
     @Override
@@ -62,11 +64,16 @@ public class StatusBar extends SettingsPreferenceFragment implements
         updateQuickPulldownSummary(mQuickPulldown.getIntValue(0));
 
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
+        mDataDisabledIcon = (Preference) findPreference(KEY_DATA_DISABLED_ICON);
         mBluetoothBatteryStatus = (Preference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
 
         if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
             mQuickPulldown.setEntries(R.array.status_bar_quick_pull_down_entries_rtl);
             mQuickPulldown.setEntryValues(R.array.status_bar_quick_pull_down_values_rtl);
+        }
+
+        if (!DeviceUtils.deviceSupportsMobileData(context)) {
+            mIconsCategory.removePreference(mDataDisabledIcon);
         }
 
         if (!DeviceUtils.deviceSupportsBluetooth(context)) {
@@ -121,6 +128,9 @@ public class StatusBar extends SettingsPreferenceFragment implements
             public List<String> getNonIndexableKeys(Context context) {
                 List<String> keys = super.getNonIndexableKeys(context);
 
+                if (!DeviceUtils.deviceSupportsMobileData(context)) {
+                    keys.add(KEY_DATA_DISABLED_ICON);
+                }
                 if (!DeviceUtils.deviceSupportsBluetooth(context)) {
                     keys.add(KEY_BLUETOOTH_BATTERY_STATUS);
                 }
