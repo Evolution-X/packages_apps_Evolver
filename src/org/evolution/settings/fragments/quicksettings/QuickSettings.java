929278617ed0689176bc0496f42107ef2ca8ff47
Pranav Vashi <neobuddy89@gmail.com>
2024-05-21 16:11:44 +0900

    Evolver: Add animations to quick settings tiles [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
+++ src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
@@ -30,6 +30,7 @@ import lineageos.preference.LineageSecureSettingSwitchPreference;
 import lineageos.providers.LineageSettings;
 
 import org.evolution.settings.preferences.SystemSettingListPreference;
+import org.evolution.settings.preferences.SystemSettingSeekBarPreference;
 import org.evolution.settings.utils.DeviceUtils;
 
 @SearchIndexable
@@ -44,6 +45,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
     private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
     private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
+    private static final String KEY_TILE_ANIM_STYLE = "qs_tile_animation_style";
+    private static final String KEY_TILE_ANIM_DURATION = "qs_tile_animation_duration";
+    private static final String KEY_TILE_ANIM_INTERPOLATOR = "qs_tile_animation_interpolator";
     private static final String KEY_MISCELLANEOUS_CATEGORY = "quick_settings_miscellaneous_category";
     private static final String KEY_BLUETOOTH_AUTO_ON = "qs_bt_auto_on";
 
@@ -57,6 +61,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private LineageSecureSettingListPreference mShowBrightnessSlider;
     private LineageSecureSettingListPreference mBrightnessSliderPosition;
     private LineageSecureSettingSwitchPreference mShowAutoBrightness;
+    private SystemSettingListPreference mTileAnimationStyle;
+    private SystemSettingSeekBarPreference mTileAnimationDuration;
+    private SystemSettingListPreference mTileAnimationInterpolator;
     private PreferenceCategory mMiscellaneousCategory;
 
     @Override
@@ -74,6 +81,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mShowBrightnessSlider = (LineageSecureSettingListPreference) findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
         mBrightnessSliderPosition = (LineageSecureSettingListPreference) findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
         mShowAutoBrightness = (LineageSecureSettingSwitchPreference) findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
+        mTileAnimationStyle = (SystemSettingListPreference) findPreference(KEY_TILE_ANIM_STYLE);
+        mTileAnimationDuration = (SystemSettingSeekBarPreference) findPreference(KEY_TILE_ANIM_DURATION);
+        mTileAnimationInterpolator = (SystemSettingListPreference) findPreference(KEY_TILE_ANIM_INTERPOLATOR);
         mMiscellaneousCategory = (PreferenceCategory) findPreference(KEY_MISCELLANEOUS_CATEGORY);
 
         int batterystyle = Settings.System.getIntForUser(resolver,
@@ -98,6 +108,12 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             mInterfaceCategory.removePreference(mShowAutoBrightness);
         }
 
+        mTileAnimationStyle.setOnPreferenceChangeListener(this);
+
+        int tileAnimationStyle = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.QS_TILE_ANIMATION_STYLE, 0, UserHandle.USER_CURRENT);
+        updateTileAnimStyle(tileAnimationStyle);
+
         if (!DeviceUtils.deviceSupportsBluetooth(context)) {
             prefScreen.removePreference(mMiscellaneousCategory);
         }
@@ -118,8 +134,17 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             if (mShowAutoBrightness != null)
                 mShowAutoBrightness.setEnabled(value > 0);
             return true;
+        } else if (preference == mTileAnimationStyle) {
+            int value = Integer.parseInt((String) newValue);
+            updateTileAnimStyle(value);
+            return true;
         }
         return false;
+    }
+
+    private void updateTileAnimStyle(int tileAnimationStyle) {
+        mTileAnimationDuration.setEnabled(tileAnimationStyle != 0);
+        mTileAnimationInterpolator.setEnabled(tileAnimationStyle != 0);
     }
 
     @Override
