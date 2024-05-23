79cad163a811f04c767ed72ea3984406d01a9784
Pranav Vashi <neobuddy89@gmail.com>
2024-05-24 03:11:00 +0900

    Evolver: Add separate tunables for navbar layout customization [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/evolution/settings/EvolutionSettings.java
+++ src/org/evolution/settings/EvolutionSettings.java
@@ -5,20 +5,50 @@
 
 package org.evolution.settings;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
+import org.evolution.settings.utils.DeviceUtils;
+
 @SearchIndexable
 public class EvolutionSettings extends DashboardFragment {
 
     private static final String TAG = "EvolutionSettings";
 
+    private static final String KEY_BUTTONS_PREF = "buttons";
+
+    private Preference mButtonsPref;
+
     @Override
     protected int getPreferenceScreenResId() {
         return R.xml.evolution_settings;
+    }
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        final Context context = getContext();
+        final ContentResolver resolver = context.getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+        final Resources resources = context.getResources();
+
+        mButtonsPref = (Preference) findPreference(KEY_BUTTONS_PREF);
+
+        if (DeviceUtils.isEdgeToEdgeEnabled(context)) {
+            prefScreen.removePreference(mButtonsPref);
+        }
     }
 
     @Override
