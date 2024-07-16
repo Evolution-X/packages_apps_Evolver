a4202da78d513524aa09327dcc4a2b325a5001b8
Joey Huab <jhuab@edukasyon.ph>
2024-07-17 08:13:07 +0900

    [SQUASH] Evolver: Remove Battery styles customizations [2/2]
    Revert "Evolver: Add Origami battery icon [2/2]"
    
    This reverts commit 92b332b389ed62c731a4bb9677d106f87088b916.
    
    Revert "Evolver: Battery styles: Add Landscape iOS battery styles [2/2]"
    
    This reverts commit b80fc92ab54b4880e455f7e6a242cdbd5df0b9c9.
    
    Revert "Evolver: Battery Styles: Added 9 more battery styles [2/2]"
    
    This reverts commit 355567c8d786766da245901284aacc57ff30059b.
    
    Revert "Evolver: Battery Styles: Add back big dotted and big circle battery indicator [2/2]"
    
    This reverts commit 80f098f84c55bc7ae6d12e1f3dbb3861a3b3bf92.
    
    Revert "Evolver: Battery styles customizations [2/2]"
    
    This reverts commit adfb0fd053752cf7773b36a9eb7d7e22f2c1840d.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
+++ src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
@@ -45,8 +45,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String TAG = "QuickSettings";
 
     private static final String KEY_INTERFACE_CATEGORY = "quick_settings_interface_category";
-    private static final String KEY_BATTERY_STYLE = "qs_battery_style";
-    private static final String KEY_BATTERY_PERCENT = "qs_show_battery_percent";
     private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
     private static final String KEY_BRIGHTNESS_SLIDER_POSITION = "qs_brightness_slider_position";
     private static final String KEY_SHOW_AUTO_BRIGHTNESS = "qs_show_auto_brightness";
@@ -66,13 +64,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String QS_SPLIT_SHADE_CUTOUT_PKG = "android.landscape.split_shade_cutout";
     private static final String QS_SPLIT_SHADE_CUTOUT_TARGET = "android";
 
-    private static final int BATTERY_STYLE_PORTRAIT = 0;
-    private static final int BATTERY_STYLE_TEXT = 4;
-    private static final int BATTERY_STYLE_HIDDEN = 5;
-
     private PreferenceCategory mInterfaceCategory;
-    private SystemSettingListPreference mBatteryStyle;
-    private SystemSettingListPreference mBatteryPercent;
     private LineageSecureSettingListPreference mShowBrightnessSlider;
     private LineageSecureSettingListPreference mBrightnessSliderPosition;
     private LineageSecureSettingSwitchPreference mShowAutoBrightness;
@@ -98,8 +90,6 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
 
-        mBatteryStyle = (SystemSettingListPreference) findPreference(KEY_BATTERY_STYLE);
-        mBatteryPercent = (SystemSettingListPreference) findPreference(KEY_BATTERY_PERCENT);
         mShowBrightnessSlider = (LineageSecureSettingListPreference) findPreference(KEY_SHOW_BRIGHTNESS_SLIDER);
         mBrightnessSliderPosition = (LineageSecureSettingListPreference) findPreference(KEY_BRIGHTNESS_SLIDER_POSITION);
         mShowAutoBrightness = (LineageSecureSettingSwitchPreference) findPreference(KEY_SHOW_AUTO_BRIGHTNESS);
@@ -108,15 +98,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mTileAnimationInterpolator = (SystemSettingListPreference) findPreference(KEY_TILE_ANIM_INTERPOLATOR);
         mMiscellaneousCategory = (PreferenceCategory) findPreference(KEY_MISCELLANEOUS_CATEGORY);
 
-        int batterystyle = Settings.System.getIntForUser(resolver,
-                Settings.System.QS_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-
-        mBatteryStyle.setOnPreferenceChangeListener(this);
-
-        mBatteryPercent.setEnabled(
-                batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
-
-       mShowBrightnessSlider.setOnPreferenceChangeListener(this);
+        mShowBrightnessSlider.setOnPreferenceChangeListener(this);
         boolean showSlider = LineageSettings.Secure.getIntForUser(resolver,
                 LineageSettings.Secure.QS_SHOW_BRIGHTNESS_SLIDER, 1, UserHandle.USER_CURRENT) > 0;
 
@@ -158,12 +140,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
-        if (preference == mBatteryStyle) {
-            int value = Integer.parseInt((String) newValue);
-            mBatteryPercent.setEnabled(
-                    value != BATTERY_STYLE_TEXT && value != BATTERY_STYLE_HIDDEN);
-            return true;
-        } else if (preference == mShowBrightnessSlider) {
+        if (preference == mShowBrightnessSlider) {
             int value = Integer.parseInt((String) newValue);
             mBrightnessSliderPosition.setEnabled(value > 0);
             if (mShowAutoBrightness != null)
