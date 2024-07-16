cc5129f42ee97e6786acd41b657748eefa5cb41e
Joey Huab <jhuab@edukasyon.ph>
2024-07-17 08:13:07 +0900

    [SQUASH] Evolver: Remove Battery styles customizations [2/2]
    Revert "Evolver: Add Origami battery icon [2/2]"
    
    This reverts commit 92b332b389ed62c731a4bb9677d106f87088b916.
    
    Revert "Evolver: Battery styles: Add Landscape iOS battery styles [2/2]"
    
    This reverts commit b80fc92ab54b4880e455f7e6a242cdbd5df0b9c9.
    
    Revert "Evolver: Battery Styles: Added 9 more battery styles [2/2]"
    
    This reverts commit 355567c8d786766da245901284aacc57ff30059b.
    
    Revert "Evolver: Battery Styles: Add back big dotted and big circle battery indicator [2/2]"
    
    This reverts commit 80f098f84c55bc7ae6d12e1f3dbb3861a3b3bf92.
    
    Revert "Evolver: Battery styles customizations [2/2]"
    
    This reverts commit adfb0fd053752cf7773b36a9eb7d7e22f2c1840d.
    
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- src/org/evolution/settings/fragments/statusbar/StatusBar.java
+++ src/org/evolution/settings/fragments/statusbar/StatusBar.java
@@ -9,8 +9,6 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.provider.Settings;
 import android.view.View;
 
 import androidx.preference.Preference;
@@ -28,7 +26,6 @@ import java.util.List;
 
 import lineageos.preference.LineageSystemSettingListPreference;
 
-import org.evolution.settings.preferences.SystemSettingListPreference;
 import org.evolution.settings.preferences.SystemSettingSwitchPreference;
 import org.evolution.settings.utils.DeviceUtils;
 
@@ -39,21 +36,11 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String TAG = "StatusBar";
 
     private static final String KEY_ICONS_CATEGORY = "status_bar_icons_category";
-    private static final String KEY_BATTERY_STYLE = "status_bar_battery_style";
-    private static final String KEY_BATTERY_PERCENT = "status_bar_show_battery_percent";
-    private static final String KEY_BATTERY_TEXT_CHARGING = "status_bar_battery_text_charging";
     private static final String KEY_DATA_DISABLED_ICON = "data_disabled_icon";
     private static final String KEY_BLUETOOTH_BATTERY_STATUS = "bluetooth_show_battery";
     private static final String KEY_FOUR_G_ICON = "show_fourg_icon";
 
-    private static final int BATTERY_STYLE_PORTRAIT = 0;
-    private static final int BATTERY_STYLE_TEXT = 4;
-    private static final int BATTERY_STYLE_HIDDEN = 5;
-
     private PreferenceCategory mIconsCategory;
-    private SystemSettingListPreference mBatteryPercent;
-    private SystemSettingListPreference mBatteryStyle;
-    private SystemSettingSwitchPreference mBatteryTextCharging;
     private SystemSettingSwitchPreference mDataDisabledIcon;
     private SystemSettingSwitchPreference mFourgIcon;
     private SystemSettingSwitchPreference mBluetoothBatteryStatus;
@@ -69,27 +56,9 @@ public class StatusBar extends SettingsPreferenceFragment implements
         final Resources resources = context.getResources();
 
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
-        mBatteryStyle = (SystemSettingListPreference) findPreference(KEY_BATTERY_STYLE);
-        mBatteryPercent = (SystemSettingListPreference) findPreference(KEY_BATTERY_PERCENT);
-        mBatteryTextCharging = (SystemSettingSwitchPreference) findPreference(KEY_BATTERY_TEXT_CHARGING);
-        mBluetoothBatteryStatus = (SystemSettingSwitchPreference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
         mDataDisabledIcon = (SystemSettingSwitchPreference) findPreference(KEY_DATA_DISABLED_ICON);
         mFourgIcon = (SystemSettingSwitchPreference) findPreference(KEY_FOUR_G_ICON);
         mBluetoothBatteryStatus = (SystemSettingSwitchPreference) findPreference(KEY_BLUETOOTH_BATTERY_STATUS);
-
-        int batterystyle = Settings.System.getIntForUser(resolver,
-                Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-        int batterypercent = Settings.System.getIntForUser(resolver,
-                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
-
-        mBatteryStyle.setOnPreferenceChangeListener(this);
-
-        mBatteryPercent.setEnabled(
-                batterystyle != BATTERY_STYLE_TEXT && batterystyle != BATTERY_STYLE_HIDDEN);
-        mBatteryPercent.setOnPreferenceChangeListener(this);
-
-        mBatteryTextCharging.setEnabled(batterystyle == BATTERY_STYLE_HIDDEN ||
-                (batterystyle != BATTERY_STYLE_TEXT && batterypercent != 2));
 
         if (!DeviceUtils.deviceSupportsMobileData(context)) {
             mIconsCategory.removePreference(mDataDisabledIcon);
@@ -105,23 +74,6 @@ public class StatusBar extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
-        if (preference == mBatteryStyle) {
-            int value = Integer.parseInt((String) newValue);
-            int batterypercent = Settings.System.getIntForUser(resolver,
-                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0, UserHandle.USER_CURRENT);
-            mBatteryPercent.setEnabled(
-                    value != BATTERY_STYLE_TEXT && value != BATTERY_STYLE_HIDDEN);
-            mBatteryTextCharging.setEnabled(value == BATTERY_STYLE_HIDDEN ||
-                    (value != BATTERY_STYLE_TEXT && batterypercent != 2));
-            return true;
-        } else if (preference == mBatteryPercent) {
-            int value = Integer.parseInt((String) newValue);
-            int batterystyle = Settings.System.getIntForUser(resolver,
-                    Settings.System.STATUS_BAR_BATTERY_STYLE, BATTERY_STYLE_PORTRAIT, UserHandle.USER_CURRENT);
-            mBatteryTextCharging.setEnabled(batterystyle == BATTERY_STYLE_HIDDEN ||
-                    (batterystyle != BATTERY_STYLE_TEXT && value != 2));
-            return true;
-        }
         return false;
     }
 
