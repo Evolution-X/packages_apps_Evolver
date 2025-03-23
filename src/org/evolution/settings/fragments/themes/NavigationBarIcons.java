a4210afd99ce260f47d0a528d2a4fb7934f318e2
Pranav Vashi <neobuddy89@gmail.com>
2025-03-23 17:12:20 +0530

    Evolver: Remove retired navbar features
    Revert "Evolver: Add separate tunables for navbar layout customization [2/2]"
    
    This reverts commit 575212bf58c30e75540e109526a63337130df73b.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/evolution/settings/fragments/themes/NavigationBarIcons.java
+++ src/org/evolution/settings/fragments/themes/NavigationBarIcons.java
@@ -77,7 +77,6 @@ public class NavigationBarIcons extends SettingsPreferenceFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getActivity().setTitle(R.string.themes_navigation_bar_icons_title);
 
         mThemeUtils = new ThemeUtils(getActivity());
         mPkgs = mThemeUtils.getOverlayPackagesForCategory(mCategory, "com.android.launcher3");
@@ -155,8 +154,6 @@ public class NavigationBarIcons extends SettingsPreferenceFragment {
                     updateActivatedStatus(navPkg, true);
                     mSelectedPkg = navPkg;
                     enableOverlays(position);
-                    Settings.System.putStringForUser(getContext().getContentResolver(),
-                            Settings.System.NAVBAR_STYLE, navPkg, UserHandle.USER_CURRENT);
                 }
             });
         }
