9c9bcfe78de274cab395ccb8cc7a1765689d325e
Joey Huab <jhuab@edukasyon.ph>
2025-06-24 12:28:42 +0900

    Evolver: Drop features for A16

--- src/org/evolution/settings/fragments/notifications/Notifications.java
+++ src/org/evolution/settings/fragments/notifications/Notifications.java
@@ -22,11 +22,9 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
-import java.util.List;
-
-import org.evolution.settings.preferences.SystemPropertySwitchPreference;
 import org.evolution.settings.preferences.SystemSettingSwitchPreference;
-import org.evolution.settings.utils.SystemUtils;
+
+import java.util.List;
 
 @SearchIndexable
 public class Notifications extends SettingsPreferenceFragment implements
@@ -36,11 +34,9 @@ public class Notifications extends SettingsPreferenceFragment implements
 
     private static final String KEY_ALERT_SLIDER_PREF = "alert_slider_notifications";
     private static final String KEY_INTERFACE_CATEGORY = "notifications_interface_category";
-    private static final String KEY_COMPACT_HUN = "persist.sys.compact_heads_up_notification.always_show";
 
     private PreferenceCategory mInterfaceCategory;
     private Preference mAlertSlider;
-    private SystemPropertySwitchPreference mCompactHUN;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,19 +55,12 @@ public class Notifications extends SettingsPreferenceFragment implements
         if (!mAlertSliderAvailable) {
             mInterfaceCategory.removePreference(mAlertSlider);
         }
-
-        mCompactHUN = (SystemPropertySwitchPreference) findPreference(KEY_COMPACT_HUN);
-        mCompactHUN.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
-        if (preference == mCompactHUN) {
-            SystemUtils.showSystemUiRestartDialog(context);
-            return true;
-        }
         return false;
     }
 
