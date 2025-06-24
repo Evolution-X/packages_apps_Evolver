7422d4fb6ad0600d83499508b2b0d155779e3536
Joey Huab <jhuab@edukasyon.ph>
2025-06-24 12:28:42 +0900

    Evolver: Drop features for A16

--- src/org/evolution/settings/fragments/themes/Themes.java
+++ src/org/evolution/settings/fragments/themes/Themes.java
@@ -8,7 +8,7 @@ package org.evolution.settings.fragments.themes;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
-import android.hardware.fingerprint.FingerprintManager;
+// import android.hardware.fingerprint.FingerprintManager;
 import android.provider.Settings;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -44,9 +44,9 @@ public class Themes extends SettingsPreferenceFragment implements
     private static final String KEY_UNLOCK_SOUND = "unlock_sound";
     private static final String KEY_ICONS_CATEGORY = "themes_icons_category";
     private static final String KEY_SIGNAL_ICON = "android.theme.customization.signal_icon";
-    private static final String KEY_UDFPS_ICON = "udfps_icon";
+//    private static final String KEY_UDFPS_ICON = "udfps_icon";
     private static final String KEY_ANIMATIONS_CATEGORY = "themes_animations_category";
-    private static final String KEY_UDFPS_ANIMATION = "udfps_animation";
+//    private static final String KEY_UDFPS_ANIMATION = "udfps_animation";
     private static final String KEY_PGB_STYLE = "progress_bar_style";
     private static final String KEY_NOTIF_STYLE = "notification_style";
     private static final String KEY_POWERMENU_STYLE = "powermenu_style";
@@ -79,9 +79,9 @@ public class Themes extends SettingsPreferenceFragment implements
     private PreferenceCategory mLauncherCategory;
     private PreferenceCategory mIconsCategory;
     private Preference mSignalIcon;
-    private Preference mUdfpsIcon;
+//    private Preference mUdfpsIcon;
     private PreferenceCategory mAnimationsCategory;
-    private Preference mUdfpsAnimation;
+//    private Preference mUdfpsAnimation;
     private SystemSettingListPreference mNotificationStylePref;
     private SystemSettingListPreference mPowerMenuStylePref;
     private SystemSettingListPreference mProgressBarPref;
@@ -105,28 +105,28 @@ public class Themes extends SettingsPreferenceFragment implements
         mLauncherCategory = (PreferenceCategory) findPreference(KEY_LAUNCHER_CATEGORY);
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
         mSignalIcon = (Preference) findPreference(KEY_SIGNAL_ICON);
-        mUdfpsIcon = (Preference) findPreference(KEY_UDFPS_ICON);
+//        mUdfpsIcon = (Preference) findPreference(KEY_UDFPS_ICON);
         mAnimationsCategory = (PreferenceCategory) findPreference(KEY_ANIMATIONS_CATEGORY);
-        mUdfpsAnimation = (Preference) findPreference(KEY_UDFPS_ANIMATION);
+//        mUdfpsAnimation = (Preference) findPreference(KEY_UDFPS_ANIMATION);
 
         if (!DeviceUtils.deviceSupportsMobileData(context)) {
             mIconsCategory.removePreference(mSignalIcon);
         }
 
-        FingerprintManager fingerprintManager = (FingerprintManager)
-                getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
+//        FingerprintManager fingerprintManager = (FingerprintManager)
+//                getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
 
-        if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
-            mIconsCategory.removePreference(mUdfpsIcon);
-            mAnimationsCategory.removePreference(mUdfpsAnimation);
-        } else {
-            if (!Utils.isPackageInstalled(context, "org.evolution.udfps.icons")) {
-                mIconsCategory.removePreference(mUdfpsIcon);
-            }
-            if (!Utils.isPackageInstalled(context, "org.evolution.udfps.animations")) {
-                mAnimationsCategory.removePreference(mUdfpsAnimation);
-            }
-        }
+//        if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+//            mIconsCategory.removePreference(mUdfpsIcon);
+//            mAnimationsCategory.removePreference(mUdfpsAnimation);
+//        } else {
+//            if (!Utils.isPackageInstalled(context, "org.evolution.udfps.icons")) {
+//                mIconsCategory.removePreference(mUdfpsIcon);
+//            }
+//            if (!Utils.isPackageInstalled(context, "org.evolution.udfps.animations")) {
+//                mAnimationsCategory.removePreference(mUdfpsAnimation);
+//            }
+//        }
 
         mProgressBarPref = findPreference(KEY_PGB_STYLE);
         mProgressBarPref.setOnPreferenceChangeListener(this);
@@ -224,8 +224,8 @@ public class Themes extends SettingsPreferenceFragment implements
                 List<String> keys = super.getNonIndexableKeys(context);
                 final Resources resources = context.getResources();
 
-                FingerprintManager fingerprintManager = (FingerprintManager)
-                        context.getSystemService(Context.FINGERPRINT_SERVICE);
+//                FingerprintManager fingerprintManager = (FingerprintManager)
+//                        context.getSystemService(Context.FINGERPRINT_SERVICE);
 
                 if (!DeviceUtils.deviceSupportsMobileData(context)) {
                     keys.add(KEY_SIGNAL_ICON);
@@ -235,17 +235,17 @@ public class Themes extends SettingsPreferenceFragment implements
                     keys.add(KEY_LAUNCHER_CATEGORY);
                 }
 
-                if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
-                    keys.add(KEY_UDFPS_ICON);
-                    keys.add(KEY_UDFPS_ANIMATION);
-                } else {
-                    if (!Utils.isPackageInstalled(context, "org.evolution.udfps.icons")) {
-                        keys.add(KEY_UDFPS_ICON);
-                    }
-                    if (!Utils.isPackageInstalled(context, "org.evolution.udfps.animations")) {
-                        keys.add(KEY_UDFPS_ANIMATION);
-                    }
-                }
+//                if (fingerprintManager == null || !fingerprintManager.isHardwareDetected()) {
+//                    keys.add(KEY_UDFPS_ICON);
+//                    keys.add(KEY_UDFPS_ANIMATION);
+//                } else {
+//                    if (!Utils.isPackageInstalled(context, "org.evolution.udfps.icons")) {
+//                        keys.add(KEY_UDFPS_ICON);
+//                    }
+//                    if (!Utils.isPackageInstalled(context, "org.evolution.udfps.animations")) {
+//                        keys.add(KEY_UDFPS_ANIMATION);
+//                    }
+//                }
                 return keys;
             }
         };
