692f7c30bbb4022472e2dec77dc696e4b73c8bc5
Pranav Vashi <neobuddy89@gmail.com>
2025-10-18 17:50:08 +0200

    Evolver: Add prop to allow blocking cert chain

--- src/org/evolution/settings/fragments/miscellaneous/Spoofing.java
+++ src/org/evolution/settings/fragments/miscellaneous/Spoofing.java
@@ -37,6 +37,7 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.evolution.KeyProviderManager;
 import com.android.internal.util.evolution.SystemRestartUtils;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -75,6 +76,7 @@ public class Spoofing extends SettingsPreferenceFragment implements
     private static final String KEY_SYSTEM_WIDE_CATEGORY = "spoofing_system_wide_category";
     private static final String KEY_UPDATE_JSON_BUTTON = "update_pif_json";
     private static final String SYS_GMS_SPOOF = "persist.sys.pixelprops.gms";
+    private static final String SYS_GMS_CERT_SPOOF = "persist.sys.pixelprops.gmscertchain";
     private static final String SYS_GOOGLE_SPOOF = "persist.sys.pixelprops";
     private static final String SYS_GAMEPROP_SPOOF = "persist.sys.pixelprops.games";
     private static final String SYS_GPHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
@@ -88,6 +90,7 @@ public class Spoofing extends SettingsPreferenceFragment implements
     private Preference mPifJsonFilePreference;
     private Preference mUpdateJsonButton;
     private PreferenceCategory mSystemWideCategory;
+    private SystemPropertySwitchPreference mDisableForceIntegrity;
     private SystemPropertySwitchPreference mGmsSpoof;
     private SystemPropertySwitchPreference mGoogleSpoof;
     private SystemPropertySwitchPreference mGamePropsSpoof;
@@ -143,18 +146,26 @@ public class Spoofing extends SettingsPreferenceFragment implements
         mSnapSpoof.setOnPreferenceChangeListener(this);
         mTensorSpoof.setOnPreferenceChangeListener(this);
 
-        mKeyboxFilePickerLauncher = registerForActivityResult(
-        new ActivityResultContracts.StartActivityForResult(),
-        result -> {
-            if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
-            Uri uri = result.getData().getData();
-            Preference pref = findPreference(KEYBOX_DATA_KEY);
-            if (pref instanceof KeyboxDataPreference) {
-                ((KeyboxDataPreference) pref).handleFileSelected(uri);
-            }
+        mDisableForceIntegrity = findPreference(SYS_GMS_CERT_SPOOF);
+        if (mDisableForceIntegrity != null) {
+            mDisableForceIntegrity.setEnabled(KeyProviderManager.isKeyboxAvailable());
         }
-    }
-    );
+
+        mKeyboxFilePickerLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    Uri uri = result.getData().getData();
+                    Preference pref = findPreference(KEYBOX_DATA_KEY);
+                    if (pref instanceof KeyboxDataPreference) {
+                        ((KeyboxDataPreference) pref).handleFileSelected(uri);
+                    }
+                    if (mDisableForceIntegrity != null) {
+                        mDisableForceIntegrity.setEnabled(KeyProviderManager.isKeyboxAvailable());
+                    }
+                }
+            }
+        );
 
         mPifJsonFilePreference.setOnPreferenceClickListener(preference -> {
             openFileSelector(10001);
