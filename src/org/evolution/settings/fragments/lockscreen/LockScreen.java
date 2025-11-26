5918453177ce366e65c3ab523bf39c25f981cf8b
Joey <joey@evolution-x.org>
2025-11-26 17:26:03 +0900

    Evolver: Clean up for Android 16 QPR1
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/evolution/settings/fragments/lockscreen/LockScreen.java
+++ src/org/evolution/settings/fragments/lockscreen/LockScreen.java
@@ -24,6 +24,7 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.android.OmniJawsClient;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
@@ -34,6 +35,7 @@ import java.util.List;
 import org.evolution.settings.preferences.SecureSettingSwitchPreference;
 import org.evolution.settings.utils.DeviceUtils;
 // import org.evolution.settings.utils.ImageUtils;
+import org.evolution.settings.utils.SystemUtils;
 
 @SearchIndexable
 public class LockScreen extends SettingsPreferenceFragment implements
@@ -43,6 +45,8 @@ public class LockScreen extends SettingsPreferenceFragment implements
 
     private static final String LOCKSCREEN_GESTURES_CATEGORY = "lockscreen_gestures_category";
     private static final String KEY_RIPPLE_EFFECT = "enable_ripple_effect";
+    private static final String KEY_SMARTSPACE = "lockscreen_smartspace_enabled";
+    private static final String KEY_WEATHER = "lockscreen_weather_enabled";
     private static final String KEY_FP_SUCCESS = "fp_success_vibrate";
     private static final String KEY_FP_ERROR = "fp_error_vibrate";
 //    private static final String CUSTOM_IMAGE_REQUEST_CODE_KEY = "lockscreen_custom_image";
@@ -50,6 +54,8 @@ public class LockScreen extends SettingsPreferenceFragment implements
 
 //    private Preference mCustomImagePreference;
     private Preference mRippleEffect;
+    private SwitchPreferenceCompat mSmartspace;
+    private SwitchPreferenceCompat mWeather;
     private SwitchPreferenceCompat mFpSuccessVib;
     private SwitchPreferenceCompat mFpErrorVib;
 
@@ -89,12 +95,30 @@ public class LockScreen extends SettingsPreferenceFragment implements
             gestCategory.removePreference(mFpSuccessVib);
             gestCategory.removePreference(mFpErrorVib);
         }
+
+        mSmartspace = (SwitchPreferenceCompat) findPreference(KEY_SMARTSPACE);
+        mSmartspace.setOnPreferenceChangeListener(this);
+
+        mWeather = (SwitchPreferenceCompat) findPreference(KEY_WEATHER);
+        mWeather.setOnPreferenceChangeListener(this);
+
+        updateWeatherSettings();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
+        if (preference == mSmartspace) {
+            mSmartspace.setChecked((Boolean)newValue);
+            updateWeatherSettings();
+            SystemUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        } else if (preference == mWeather) {
+            mWeather.setChecked((Boolean)newValue);
+            SystemUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        }
         return false;
     }
 
@@ -128,6 +152,21 @@ public class LockScreen extends SettingsPreferenceFragment implements
 //            }
 //        }
 //    }
+
+    private void updateWeatherSettings() {
+        if (mWeather == null || mSmartspace == null) return;
+
+        boolean weatherEnabled = OmniJawsClient.get().isOmniJawsEnabled(getContext());
+        mWeather.setEnabled(!mSmartspace.isChecked() && weatherEnabled);
+        mWeather.setSummary(!mSmartspace.isChecked() && weatherEnabled ? R.string.lockscreen_weather_summary :
+            R.string.lockscreen_weather_enabled_info);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateWeatherSettings();
+    }
 
     @Override
     public int getMetricsCategory() {
--- src/org/evolution/settings/fragments/statusbar/Clock.java
+++ src/org/evolution/settings/fragments/statusbar/Clock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016-2024 crDroid Android Project
+ * Copyright (C) 2016-2025 crDroid Android Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,7 +26,6 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.format.DateFormat;
 import android.view.Menu;
-import android.view.View;
 import android.widget.EditText;
 
 import androidx.preference.ListPreference;
@@ -41,14 +40,12 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
 import java.util.Date;
-import java.util.List;
 
 import lineageos.preference.LineageSystemSettingListPreference;
 import lineageos.providers.LineageSettings;
 
 import org.evolution.settings.preferences.CustomSeekBarPreference;
 import org.evolution.settings.preferences.SystemSettingListPreference;
-import org.evolution.settings.utils.DeviceUtils;
 
 @SearchIndexable
 public class Clock extends SettingsPreferenceFragment implements
@@ -56,7 +53,6 @@ public class Clock extends SettingsPreferenceFragment implements
 
     private static final String TAG = "Clock";
 
-    private static final String STATUS_BAR_CLOCK_POSITION = "status_bar_clock";
     private static final String STATUS_BAR_AM_PM = "status_bar_am_pm";
     private static final String CLOCK_DATE_DISPLAY = "status_bar_clock_date_display";
     private static final String CLOCK_DATE_POSITION = "status_bar_clock_date_position";
@@ -67,7 +63,6 @@ public class Clock extends SettingsPreferenceFragment implements
     private static final int CLOCK_DATE_STYLE_UPPERCASE = 2;
     private static final int CUSTOM_CLOCK_DATE_FORMAT_INDEX = 18;
 
-    private LineageSystemSettingListPreference mClockPosition;
     private LineageSystemSettingListPreference mStatusBarAmPm;
     private SystemSettingListPreference mClockDateDisplay;
     private SystemSettingListPreference mClockDatePosition;
@@ -81,23 +76,6 @@ public class Clock extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.status_bar_clock);
 
         ContentResolver resolver = getActivity().getContentResolver();
-        Context mContext = getActivity().getApplicationContext();
-
-        mClockPosition =
-                (LineageSystemSettingListPreference) findPreference(STATUS_BAR_CLOCK_POSITION);
-
-        if (getResources().getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL) {
-            if (DeviceUtils.hasCenteredCutout(mContext)) {
-                mClockPosition.setEntries(R.array.status_bar_clock_position_entries_notch_rtl);
-                mClockPosition.setEntryValues(R.array.status_bar_clock_position_values_notch_rtl);
-            } else {
-                mClockPosition.setEntries(R.array.status_bar_clock_position_entries_rtl);
-                mClockPosition.setEntryValues(R.array.status_bar_clock_position_values_rtl);
-            }
-        } else if (DeviceUtils.hasCenteredCutout(mContext)) {
-            mClockPosition.setEntries(R.array.status_bar_clock_position_entries_notch);
-            mClockPosition.setEntryValues(R.array.status_bar_clock_position_values_notch);
-        }
 
         mStatusBarAmPm =
                 (LineageSystemSettingListPreference) findPreference(STATUS_BAR_AM_PM);
@@ -237,12 +215,5 @@ public class Clock extends SettingsPreferenceFragment implements
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-        new BaseSearchIndexProvider(R.xml.status_bar_clock) {
-
-            @Override
-            public List<String> getNonIndexableKeys(Context context) {
-                List<String> keys = super.getNonIndexableKeys(context);
-                return keys;
-            }
-        };
+        new BaseSearchIndexProvider(R.xml.status_bar_clock);
 }
