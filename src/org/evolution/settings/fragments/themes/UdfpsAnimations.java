b8fe973c58c1e36bdfe944aa86c0d3d3f478e090
Bruno Martins <bgcngm@gmail.com>
2025-11-26 20:21:00 +0900

    Evolver: Defer applying divider
    Android 16 QPR1 + M3E changes the inflation and lifecycle timing of
    PreferenceFragmentCompat layouts, resulting in internal RecyclerView
    being created later than before.
    
    As a result, PreferenceFragmentCompat.setDivider() can be invoked
    before the internal RecyclerView exists thus causing below NPE:
    
    https://paste.evolution-x.org/foaHLQ
    
    Test: Open System profiles settings, create a new profile and verify
          that the activity no longer crashes.
    Change-Id: Ifb741011baaa6d721d0ca0db2d68d30037eb8930

--- src/org/evolution/settings/fragments/themes/UdfpsAnimations.java
+++ src/org/evolution/settings/fragments/themes/UdfpsAnimations.java
@@ -104,6 +104,20 @@ public class UdfpsAnimations extends SettingsPreferenceFragment {
     }
 
     @Override
+    public void setDivider(@Nullable final Drawable divider) {
+        RecyclerView list = getListView();
+        if (list == null) {
+            View root = getView();
+            if (root != null) {
+                root.post(() -> setDivider(divider));
+            }
+            return;
+        }
+
+        super.setDivider(divider);
+    }
+
+    @Override
     public int getMetricsCategory() {
         return MetricsEvent.EVOLVER;
     }
