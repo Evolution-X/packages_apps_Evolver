e0f6ba15672903266167827d2be94e45c4b93171
Ghosuto <clash.raja10@gmail.com>
2026-02-06 09:19:36 +0000

    Evolver: Allow switching QS tile color to pre-qpr shade [2/2]
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>

--- src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
+++ src/org/evolution/settings/fragments/quicksettings/QuickSettings.java
@@ -65,6 +65,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private static final String KEY_QS_TILE_LABEL_HIDE = "qs_tile_label_hide";
     private static final String KEY_QS_SHOW_MEDIA_PLAYER = "qs_show_media_player";
     private static final String KEY_SINGLE_QS_TONE_ENABLED = "single_qs_tone_enabled";
+    private static final String KEY_QS_TILE_ALTERNATE_COLOR = "qs_tile_alternate_color";
 
 //    private static final int BATTERY_STYLE_PORTRAIT = 0;
 //    private static final int BATTERY_STYLE_TEXT = 4;
@@ -77,6 +78,7 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     private SwitchPreferenceCompat mBrightnessSliderHaptic;
     private SwitchPreferenceCompat mShowAutoBrightness;
     private SwitchPreferenceCompat mQsTileHaptic;
+    private SwitchPreferenceCompat mQsTileAlternateColor;
     private ListPreference mQsPanelStyle;
     private Preference mQsTileShape;
     private Preference mQsTileIconShape;
@@ -123,6 +125,11 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         mSingleQsToneEnabled = (SystemSettingSwitchPreference) findPreference(KEY_SINGLE_QS_TONE_ENABLED);
         if (mSingleQsToneEnabled != null) {
             mSingleQsToneEnabled.setOnPreferenceChangeListener(this);
+        }
+
+        mQsTileAlternateColor = findPreference(KEY_QS_TILE_ALTERNATE_COLOR);
+        if (mQsTileAlternateColor != null) {
+            mQsTileAlternateColor.setOnPreferenceChangeListener(this);
         }
 
         mMediaWaveformSeekBar = (SystemSettingSwitchPreference) findPreference(KEY_MEDIA_WAVEFORM_SEEKBAR);
@@ -215,6 +222,9 @@ public class QuickSettings extends SettingsPreferenceFragment implements
             SystemUtils.showSystemUiRestartDialog(getActivity());
             return true;
         } else if (preference == mSingleQsToneEnabled) {
+            SystemUtils.showSystemUiRestartDialog(getActivity());
+            return true;
+        } else if (preference == mQsTileAlternateColor) {
             SystemUtils.showSystemUiRestartDialog(getActivity());
             return true;
         } else if (preference == mMediaWaveformSeekBar) {
