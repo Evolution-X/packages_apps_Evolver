e8411d1f9d0b7a64cb1552de8576488321476a7c
Ido Ben-Hur <idoybh2@gmail.com>
2026-02-09 00:06:23 +0900

    Evolver: Add Flashlight blink on incoming calls [3/3]
    * Also includes "Add DND & Rate settings to flash on call"
    
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/org/evolution/settings/fragments/notifications/Notifications.java
+++ src/org/evolution/settings/fragments/notifications/Notifications.java
@@ -10,13 +10,18 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.provider.Settings;
 
+import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.evolution.Utils;
+
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
@@ -24,7 +29,7 @@ import com.android.settingslib.search.SearchIndexable;
 
 import java.util.List;
 
-import org.evolution.settings.preferences.SystemPropertySwitchPreference;
+import org.evolution.settings.preferences.CustomSeekBarPreference;
 import org.evolution.settings.preferences.SystemSettingSwitchPreference;
 import org.evolution.settings.utils.SystemUtils;
 
@@ -34,13 +39,18 @@ public class Notifications extends SettingsPreferenceFragment implements
 
     private static final String TAG = "Notifications";
 
+    private static final String FLASHLIGHT_CATEGORY = "flashlight_category";
+    private static final String FLASHLIGHT_CALL_PREF = "flashlight_on_call";
+    private static final String FLASHLIGHT_DND_PREF = "flashlight_on_call_ignore_dnd";
+    private static final String FLASHLIGHT_RATE_PREF = "flashlight_on_call_rate";
     private static final String KEY_ALERT_SLIDER_PREF = "alert_slider_notifications";
     private static final String KEY_INTERFACE_CATEGORY = "notifications_interface_category";
-//    private static final String KEY_COMPACT_HUN = "persist.sys.compact_heads_up_notification.always_show";
 
     private PreferenceCategory mInterfaceCategory;
     private Preference mAlertSlider;
-//    private SystemPropertySwitchPreference mCompactHUN;
+    private ListPreference mFlashOnCall;
+    private SwitchPreferenceCompat mFlashOnCallIgnoreDND;
+    private CustomSeekBarPreference mFlashOnCallRate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,18 +70,38 @@ public class Notifications extends SettingsPreferenceFragment implements
             mInterfaceCategory.removePreference(mAlertSlider);
         }
 
-//        mCompactHUN = (SystemPropertySwitchPreference) findPreference(KEY_COMPACT_HUN);
-//        mCompactHUN.setOnPreferenceChangeListener(this);
+        if (!Utils.deviceHasFlashlight(mContext)) {
+            final PreferenceCategory flashlightCategory =
+                    (PreferenceCategory) prefScreen.findPreference(FLASHLIGHT_CATEGORY);
+            prefScreen.removePreference(flashlightCategory);
+        } else {
+            mFlashOnCall = (ListPreference)
+                    prefScreen.findPreference(FLASHLIGHT_CALL_PREF);
+            mFlashOnCall.setOnPreferenceChangeListener(this);
+
+            mFlashOnCallIgnoreDND = (SwitchPreferenceCompat)
+                    prefScreen.findPreference(FLASHLIGHT_DND_PREF);
+            int value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, 0);
+
+            mFlashOnCallRate = (CustomSeekBarPreference)
+                    prefScreen.findPreference(FLASHLIGHT_RATE_PREF);
+
+            mFlashOnCallIgnoreDND.setEnabled(value > 1);
+            mFlashOnCallRate.setEnabled(value > 0);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
-//        if (preference == mCompactHUN) {
-//            SystemUtils.showSystemUiRestartDialog(context);
-//            return true;
-//        }
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            mFlashOnCallIgnoreDND.setEnabled(value > 1);
+            mFlashOnCallRate.setEnabled(value > 0);
+            return true;
+        }
         return false;
     }
 
@@ -92,6 +122,12 @@ public class Notifications extends SettingsPreferenceFragment implements
                             com.android.internal.R.bool.config_hasAlertSlider);
                     if (!mAlertSliderAvailable)
                         keys.add(KEY_ALERT_SLIDER_PREF);
+
+                    if (!Utils.deviceHasFlashlight(context)) {
+                        keys.add(FLASHLIGHT_CALL_PREF);
+                        keys.add(FLASHLIGHT_DND_PREF);
+                        keys.add(FLASHLIGHT_RATE_PREF);
+                    }
 
                     return keys;
                 }
