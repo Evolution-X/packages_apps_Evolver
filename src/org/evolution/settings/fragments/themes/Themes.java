0a37beaa705faf2a872c62c610d372b9efc926db
hiroshi <hiroshi@tanuki.page>
2026-02-28 12:18:53 -0500

    Evolver: Implement option to prevent OneSearchAimActivity from being disabled [2/2]
    - Toggle will also enable and disable the activity accordingly

--- src/org/evolution/settings/fragments/themes/Themes.java
+++ src/org/evolution/settings/fragments/themes/Themes.java
@@ -11,6 +11,7 @@ import android.content.res.Resources;
 import android.hardware.fingerprint.FingerprintManager;
 import android.provider.Settings;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 
 import androidx.preference.Preference;
@@ -29,7 +30,9 @@ import com.android.settingslib.search.SearchIndexable;
 import java.util.List;
 
 import org.evolution.settings.preferences.GlobalSettingListPreference;
+import org.evolution.settings.preferences.SystemPropertySwitchPreference;
 import org.evolution.settings.preferences.SystemSettingListPreference;
+import org.evolution.settings.utils.DeviceUtils;
 import org.evolution.settings.utils.SystemUtils;
 
 @SearchIndexable
@@ -38,6 +41,10 @@ public class Themes extends SettingsPreferenceFragment implements
 
     private static final String TAG = "Themes";
 
+    private static final String VELVET_PACKAGE = "com.google.android.googlequicksearchbox";
+    private static final String VELVET_NEW_SEARCH_CLASS = VELVET_PACKAGE + ".OneSearchAimActivity";
+    private static final String VELVET_ONESEARCH_COMPONENT = VELVET_PACKAGE + "/" + VELVET_NEW_SEARCH_CLASS;
+
     private static final String KEY_LOCK_SOUND = "lock_sound";
     private static final String KEY_UNLOCK_SOUND = "unlock_sound";
     private static final String KEY_ICONS_CATEGORY = "themes_icons_category";
@@ -45,10 +52,12 @@ public class Themes extends SettingsPreferenceFragment implements
     private static final String KEY_ANIMATIONS_CATEGORY = "themes_animations_category";
     private static final String KEY_UDFPS_ANIMATION = "udfps_animation";
     private static final String KEY_LAUNCHER_CATEGORY = "themes_launcher_category";
+    private static final String KEY_LAUNCHER_SEARCH_BAR = "persist.sys.velvet.force_onesearch";
 
     private GlobalSettingListPreference mLockSound;
     private GlobalSettingListPreference mUnlockSound;
     private PreferenceCategory mLauncherCategory;
+    private SystemPropertySwitchPreference mSearchBar;
     private PreferenceCategory mIconsCategory;
     private Preference mUdfpsIcon;
     private PreferenceCategory mAnimationsCategory;
@@ -71,6 +80,7 @@ public class Themes extends SettingsPreferenceFragment implements
         mUnlockSound = (GlobalSettingListPreference) findPreference(KEY_UNLOCK_SOUND);
         mUnlockSound.setOnPreferenceChangeListener(this);
         mLauncherCategory = (PreferenceCategory) findPreference(KEY_LAUNCHER_CATEGORY);
+        mSearchBar = (SystemPropertySwitchPreference) findPreference(KEY_LAUNCHER_SEARCH_BAR);
         mIconsCategory = (PreferenceCategory) findPreference(KEY_ICONS_CATEGORY);
         mUdfpsIcon = (Preference) findPreference(KEY_UDFPS_ICON);
         mAnimationsCategory = (PreferenceCategory) findPreference(KEY_ANIMATIONS_CATEGORY);
@@ -93,6 +103,16 @@ public class Themes extends SettingsPreferenceFragment implements
 
         if (!Utils.isPackageInstalled(context, "com.google.android.apps.nexuslauncher")) {
             prefScreen.removePreference(mLauncherCategory);
+        }
+
+        if (mSearchBar != null) {
+            mSearchBar.setChecked(isOneSearchAimActivityEnabled(context)
+            && SystemProperties.getBoolean(KEY_LAUNCHER_SEARCH_BAR, false));
+            mSearchBar.setOnPreferenceClickListener(pref -> {
+                boolean enabled = mSearchBar.isChecked();
+                DeviceUtils.setComponentEnabled(context, VELVET_ONESEARCH_COMPONENT, enabled);
+                return false;
+            });
         }
     }
 
@@ -120,6 +140,10 @@ public class Themes extends SettingsPreferenceFragment implements
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.EVOLVER;
+    }
+
+    private static boolean isOneSearchAimActivityEnabled(Context context) {
+        return DeviceUtils.isActivityEnabled(context, VELVET_ONESEARCH_COMPONENT);
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
