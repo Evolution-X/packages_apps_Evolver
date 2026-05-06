0a05c4b80b01c36a7005f2685e5e8fa0055348ea
TechPanelGM <giomook2003@gmail.com>
2026-05-06 11:45:24 +0000

    Evolver: Changelog: Improve changelog color contrast
    - Adjust the text color based on the light/dark mode
    - Added a helper for the span application
    - Prevent NPE while switching theme within changelog fragment
    
    Change-Id: I12007e8716da7bf3aa06110621d358afcd61f92e

--- src/org/evolution/settings/fragments/about/ChangelogFragment.java
+++ src/org/evolution/settings/fragments/about/ChangelogFragment.java
@@ -16,6 +16,7 @@
 package org.evolution.settings.fragments.about;
 
 import android.annotation.Nullable;
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.graphics.Typeface;
@@ -46,6 +47,16 @@ public class ChangelogFragment extends SettingsPreferenceFragment {
 
     private static final String CHANGELOG_PATH = "https://raw.githubusercontent.com/Evolution-X/changelog/refs/heads/bka/changelogs/LATEST.txt";
 
+    private int getThemeColor(Context context, int attr) {
+        TypedValue typedValue = new TypedValue();
+        context.getTheme().resolveAttribute(attr, typedValue, true);
+        return context.getColor(typedValue.resourceId);
+    }
+
+    private void setSpan(SpannableStringBuilder sb, Object span, int start, int end) {
+        sb.setSpan(span, start, end, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
@@ -55,6 +66,8 @@ public class ChangelogFragment extends SettingsPreferenceFragment {
     @Override
     public void onViewCreated(final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+	final Context context = view.getContext();
 
         textView = view.findViewById(R.id.changelog_text);
 
@@ -66,6 +79,7 @@ public class ChangelogFragment extends SettingsPreferenceFragment {
             Pattern commit = Pattern.compile("([a-f0-9]{7})");
             Pattern committer = Pattern.compile("\\[(\\D.*?)]");
             Pattern title = Pattern.compile("(\\R\\s+[\\*]\\s.*)");
+	    Pattern notableRomChanges = Pattern.compile("(?m)^Notable ROM changes.*$");
 
             try {
                 char tmp[] = new char[2048];
@@ -84,29 +98,40 @@ public class ChangelogFragment extends SettingsPreferenceFragment {
             }
 
             SpannableStringBuilder sb = new SpannableStringBuilder(data);
-            Resources.Theme theme = getContext().getTheme();
-            TypedValue typedValue = new TypedValue();
-            theme.resolveAttribute(android.R.attr.colorAccent, typedValue, true);
-            final int color = getContext().getColor(typedValue.resourceId);
+
+            Resources.Theme theme = context.getTheme();
+
+ 	    final int color = getThemeColor(context, android.R.attr.colorAccent);
+     		final int textColor = getThemeColor(context, android.R.attr.textColorPrimary);
+
+            sb.setSpan(new ForegroundColorSpan(textColor), 0, sb.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
 
             Matcher m = date.matcher(data);
             while (m.find()) {
-                sb.setSpan(new ForegroundColorSpan(color), m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-                sb.setSpan(new StyleSpan(Typeface.BOLD), m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                setSpan(sb, new ForegroundColorSpan(color), m.start(1), m.end(1));
+                setSpan(sb, new StyleSpan(Typeface.BOLD), m.start(1), m.end(1));
             }
+
             m = commit.matcher(data);
             while (m.find()) {
-                sb.setSpan(new StyleSpan(Typeface.NORMAL), m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                setSpan(sb, new StyleSpan(Typeface.NORMAL), m.start(1), m.end(1));
             }
+
             m = committer.matcher(data);
             while (m.find()) {
-                sb.setSpan(new ForegroundColorSpan(color), m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-                sb.setSpan(new StyleSpan(Typeface.NORMAL), m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                setSpan(sb, new ForegroundColorSpan(color), m.start(1), m.end(1));
+                setSpan(sb, new StyleSpan(Typeface.NORMAL), m.start(1), m.end(1));
             }
+
             m = title.matcher(data);
             while (m.find()) {
-                sb.setSpan(new ForegroundColorSpan(color), m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-                sb.setSpan(new StyleSpan(Typeface.BOLD), m.start(1), m.end(1), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                setSpan(sb, new ForegroundColorSpan(color), m.start(1), m.end(1));
+                setSpan(sb, new StyleSpan(Typeface.BOLD), m.start(1), m.end(1));
+            }
+
+            m = notableRomChanges.matcher(data);
+            while (m.find()) {
+                setSpan(sb, new StyleSpan(Typeface.BOLD), m.start(), m.end());
             }
 
             final SpannableStringBuilder result = sb;
