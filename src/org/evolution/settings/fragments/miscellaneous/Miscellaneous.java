a8cad06ce9731622764a7e46f11c111bea37616a
Joey <joey@evolution-x.org>
2026-05-09 14:00:19 +0900

    Evolver: Reorganize categories and move flashlight to misc
    - Move flashlight on call settings from Notifications to
      Miscellaneous under the gestures category
    - Add deviceHasFlashlight guard and search index exclusion
      to Miscellaneous; remove from Notifications
    - Split lock screen interface category into Clock, Media,
      and Charging effects subcategories
    - Rename themes_general_category to themes_monet_colors_category
      and themes_animations_category to themes_visual_effects_category
    - Add corresponding string resources for new category titles
    - Remove dead commented-out code throughout

--- src/org/evolution/settings/fragments/miscellaneous/Miscellaneous.java
+++ src/org/evolution/settings/fragments/miscellaneous/Miscellaneous.java
@@ -11,14 +11,17 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
+import android.provider.Settings;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.internal.util.evolution.Utils;
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.SettingsPreferenceFragment;
@@ -27,6 +30,9 @@ import com.android.settingslib.search.SearchIndexable;
 import java.util.List;
 
 import lineageos.providers.LineageSettings;
+
+import org.evolution.settings.preferences.CustomSeekBarPreference;
+import org.evolution.settings.preferences.SystemSettingSwitchPreference;
 
 import static org.lineageos.internal.util.DeviceKeysConstants.*;
 
@@ -37,8 +43,14 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     private static final String TAG = "Miscellaneous";
 
     private static final String KEY_THREE_FINGERS_SWIPE = "three_fingers_swipe";
+    private static final String FLASHLIGHT_CALL_PREF = "flashlight_on_call";
+    private static final String FLASHLIGHT_DND_PREF = "flashlight_on_call_ignore_dnd";
+    private static final String FLASHLIGHT_RATE_PREF = "flashlight_on_call_rate";
 
     private ListPreference mThreeFingersSwipeAction;
+    private ListPreference mFlashOnCall;
+    private SwitchPreferenceCompat mFlashOnCallIgnoreDND;
+    private CustomSeekBarPreference mFlashOnCallRate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +66,27 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
                 LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION,
                 Action.NOTHING);
         mThreeFingersSwipeAction = initList(KEY_THREE_FINGERS_SWIPE, threeFingersSwipeAction);
+
+        if (!Utils.deviceHasFlashlight(mContext)) {
+            prefScreen.removePreference(prefScreen.findPreference(FLASHLIGHT_CALL_PREF));
+            prefScreen.removePreference(prefScreen.findPreference(FLASHLIGHT_DND_PREF));
+            prefScreen.removePreference(prefScreen.findPreference(FLASHLIGHT_RATE_PREF));
+        } else {
+            mFlashOnCall = (ListPreference)
+                    prefScreen.findPreference(FLASHLIGHT_CALL_PREF);
+            mFlashOnCall.setOnPreferenceChangeListener(this);
+
+            mFlashOnCallIgnoreDND = (SwitchPreferenceCompat)
+                    prefScreen.findPreference(FLASHLIGHT_DND_PREF);
+            int value = Settings.System.getInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, 0);
+
+            mFlashOnCallRate = (CustomSeekBarPreference)
+                    prefScreen.findPreference(FLASHLIGHT_RATE_PREF);
+
+            mFlashOnCallIgnoreDND.setEnabled(value > 1);
+            mFlashOnCallRate.setEnabled(value > 0);
+        }
     }
 
     private ListPreference initList(String key, Action value) {
@@ -85,6 +118,12 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
                     LineageSettings.System.KEY_THREE_FINGERS_SWIPE_ACTION);
             return true;
         }
+        if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            mFlashOnCallIgnoreDND.setEnabled(value > 1);
+            mFlashOnCallRate.setEnabled(value > 0);
+            return true;
+        }
         return false;
     }
 
@@ -94,5 +133,20 @@ public class Miscellaneous extends SettingsPreferenceFragment implements
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.evolution_settings_miscellaneous);
+            new BaseSearchIndexProvider(R.xml.evolution_settings_miscellaneous) {
+
+                @Override
+                public List<String> getNonIndexableKeys(Context context) {
+                    List<String> keys = super.getNonIndexableKeys(context);
+                    final Resources res = context.getResources();
+
+                    if (!Utils.deviceHasFlashlight(context)) {
+                        keys.add(FLASHLIGHT_CALL_PREF);
+                        keys.add(FLASHLIGHT_DND_PREF);
+                        keys.add(FLASHLIGHT_RATE_PREF);
+                    }
+
+                    return keys;
+                }
+            };
 }
