55ed36e24131cd604c0b214be537eb5ac6fcdfbc
Joey <joey@evolution-x.org>
2026-06-24 11:52:16 +0900

    Evolver: CP2A bringup
    Co-authored-by: Ghosuto <clash.raja10@gmail.com>
    Co-authored-by: Kujou Yuko <i@779.moe>

--- src/org/evolution/settings/fragments/statusbar/StatusbarLyricSettings.java
+++ src/org/evolution/settings/fragments/statusbar/StatusbarLyricSettings.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2024-2025 crDroid Android Project
+ * Copyright (C) 2022 Project Kaleidoscope
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,29 +13,26 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.evolution.settings.fragments.quicksettings;
+package org.evolution.settings.fragments.statusbar;
 
-import android.content.ContentResolver;
-import android.content.Context;
 import android.os.Bundle;
-import android.os.UserHandle;
-import android.provider.Settings;
+
+import androidx.preference.Preference;
 
 import com.android.internal.logging.nano.MetricsProto;
+
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 
 @SearchIndexable
-public class LayoutSettings extends SettingsPreferenceFragment {
-
-    public static final String TAG = "LayoutSettings";
+public class StatusbarLyricSettings extends SettingsPreferenceFragment {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.qs_layout_settings);
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        addPreferencesFromResource(R.xml.status_bar_lyric_settings);
     }
 
     @Override
@@ -47,5 +44,5 @@ public class LayoutSettings extends SettingsPreferenceFragment {
      * For search
      */
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-            new BaseSearchIndexProvider(R.xml.qs_layout_settings);
+            new BaseSearchIndexProvider(R.xml.status_bar_lyric_settings);
 }
